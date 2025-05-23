3e8d51ce5638319c4531e80d1962b6e835221bde
쑤언 탕_Xuan Thang <thangdx@hivelab.com>
2025-05-23 03:06:32 -0700

    Push 01

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const ovlay = document.querySelector(".header-mb .overlay");
+const menu = document.querySelector(".header-mb_area");
+const closeBtn = document.querySelector(".header-mb_area .icon_times");
+const menuBtn = document.querySelector(".header .icon_menu");
+
+// Mở menu
+menuBtn.addEventListener("click", () => {
+  menu.classList.add("is_show");
+  ovlay.classList.add("is_show");
+});
+
+// Đóng menu
+closeBtn.addEventListener("click", () => {
+  menu.classList.remove("is_show");
+  ovlay.classList.remove("is_show");
+});
+
+ovlay.addEventListener("click", () => {
+  menu.classList.remove("is_show");
+  ovlay.classList.remove("is_show");
+});
