6fc2da096a11af6ba3af4d4aed3ee18f0a880a5f
xuanthang205 <thang652014@gmail.com>
2025-05-25 11:21:38 +0700

    Push 03

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
-const ovlay = document.querySelector(".header .overlay");
-const menu = document.querySelector(".header_box");
+const menuBtn = document.querySelector(".header .menu_btn");
 const closeBtn = document.querySelector(".header .times_btn");
-const menuBtn = document.querySelector(".header .icon_menu");
+const overlay = document.querySelector(".header .overlay");
+const headerBox = document.querySelector(".header_box");
 
-// Mở menu
 menuBtn.addEventListener("click", () => {
-  menu.classList.add("is_show");
-  ovlay.classList.add("is_show");
+    overlay.classList.add("is_show");
+    headerBox.classList.add("is_show");
+
+    setTimeout(() => {
+        headerBox.style.transition = "transform 0.25s ease-in-out";
+        headerBox.classList.add("is_move");
+    }, 10);
 });
 
-// Đóng menu
-closeBtn.addEventListener("click", () => {
-  menu.classList.remove("is_show");
-  ovlay.classList.remove("is_show");
-});
+function closeMenu() {
+    headerBox.classList.remove("is_move");
+    setTimeout(() => {
+        headerBox.classList.remove("is_show");
+        overlay.classList.remove("is_show");
+        headerBox.style.removeProperty("transition");
+    }, 250);
+}
 
-ovlay.addEventListener("click", () => {
-  menu.classList.remove("is_show");
-  ovlay.classList.remove("is_show");
-});
+closeBtn.addEventListener("click", closeMenu);
+overlay.addEventListener("click", closeMenu);
